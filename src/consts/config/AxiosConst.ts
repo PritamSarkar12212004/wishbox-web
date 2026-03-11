d1e90332f96ff21dc297d3b7da8b5bc99071250e
Pritam Sarkar <pritamsarkar21122004@gmail.com>
2026-03-11 16:50:59 +0530

    production

--- src/consts/config/AxiosConst.ts
+++ src/consts/config/AxiosConst.ts
@@ -1,5 +1,5 @@
 const AxiosConst = {
-  // API_PATH: "https://wish-box-clint-admin.vercel.app",
-  API_PATH: "http://192.168.1.14:3000",
+  API_PATH: "https://wish-box-clint-admin.vercel.app",
+  // API_PATH: "http://192.168.1.14:3000",
 };
 export default AxiosConst;
